d74f8db9dc4733aff37c30568f66769cc840eaa7
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2024-05-05 10:29:51 -0600

    :heavy_plus_sign: Cookie parser

--- auto-market-api/src/app.ts
+++ auto-market-api/src/app.ts
@@ -6,11 +6,13 @@ import userRouter from './routes/user.routes'
 import authRouter from '../src/routes/auth.routes'
 import carRouter from '../src/routes/car.routes'
 import { main } from "./utils/route.main";
+import cookieParser from 'cookie-parser'
 
 const PORT = process.env.PORT || 3003;
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 connectDB();
 
