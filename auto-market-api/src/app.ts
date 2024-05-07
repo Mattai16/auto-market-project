4b1cc1bba7ad609885f955cc587ec3685bf927c0
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2024-05-07 17:12:43 -0600

    :white_check_mark: Origin cors

--- auto-market-api/src/app.ts
+++ auto-market-api/src/app.ts
@@ -13,7 +13,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+
 connectDB();
 
 app.use(`${main}/carros`, carRouter)
