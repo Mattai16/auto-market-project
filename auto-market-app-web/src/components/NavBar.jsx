4faa0b12572f69c08fb215095532a2d7cb5d3779
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2024-05-07 16:33:59 -0600

    :lipstick: Navbar

--- auto-market-app-web/src/components/NavBar.jsx
+++ auto-market-app-web/src/components/NavBar.jsx
@@ -1,11 +1,33 @@
+import { Link } from "react-router-dom";
 
 function NavBar() {
 
-  return (
-    <div>
+    return (
+        <nav className="bg-cyan-950 shadow-lg fixed w-full z-10 top-0" >
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between items-center h-16">
+                    <div className="flex-shrink-0 flex items-center">
+                        <span className="text-white text-xl font-semibold">AUTO MARKET</span>
+                    </div>
 
-    </div>
-  );
+                    <div className="flex justify-center">
+                        <ul className="flex space-x-2">
+                            <li>
+                                <Link to="/" className="text-white hover:bg-gray-500 px-3 py-2 rounded-md text-sm font-medium">INICIO</Link>
+                            </li>
+                            <li>
+                                <Link to="/carros" className="text-white hover:bg-gray-500 px-3 py-2 rounded-md text-sm font-medium">CARROS</Link>
+                            </li>
+                            <li>
+                                <Link to="/carros" className="text-white hover:bg-gray-500 px-3 py-2 rounded-md text-sm font-medium">LOGIN</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
+
 
 export default NavBar;
--- auto-market-app-web/src/pages/HomePage.jsx
+++ auto-market-app-web/src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { Link } from 'react-router-dom';
 
 function HomePage() {
-  return (
-    <div>
-        <NavBar/>
-    </div>
-  )
+
+    return (
+        <div>
+            <NavBar />
+            <main className='container mx-auto px-20 mt-20 grid grid-cols-4 gap-5'>
+
+
+            </main>
+        </div>
+    )
 }
 
 export default HomePage
