41d76e4cc8e70e6427a9de09ea030bb150297f49
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2024-05-07 12:19:54 -0600

    :heavy_minus_sign: Reactstrap

--- auto-market-app-web/src/App.js
+++ auto-market-app-web/src/App.js
@@ -1,10 +1,14 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import './index.css';
 
 function App() {
   return (
-    <div className="">
-      Hola react app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
