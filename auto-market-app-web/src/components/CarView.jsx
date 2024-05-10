203fbb4efd2c6d226b0353bb6f1acb03391f7846
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2024-05-10 12:21:40 -0600

    :lipstick: Description

--- auto-market-app-web/src/components/CarView.jsx
+++ auto-market-app-web/src/components/CarView.jsx
@@ -3,57 +3,55 @@ import React from 'react';
 const CarPublication = ({ car }) => {
     const imageUrl = `data:image/jpeg;base64,${car.image}`;
     return (
-        <div className="w-[500px] h-[510px]  mx-auto mt-10 bg-white p-8 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">Infromación del coche</h2>
+        <div className="max-w-[500px] mx-auto mt-10 bg-white rounded-lg shadow-md overflow-hidden">
+            <img className="w-full h-40 object-cover" src={imageUrl} alt={car.model} />
             <div className="flex flex-col md:flex-row gap-8">
-                <div>
-                    <img src={imageUrl} alt="Coche" className="w-full h-auto rounded-md" />
-                    <div className="col-span-2">
-                        <label className="block text-gray-700 font-bold mb-2 mt-4">Marca</label>
-                        <p>{car.brand}</p>
-                    </div>
-                    <div className="col-span-2">
-                        <label className="block text-gray-700 font-bold mb-2">Modelo</label>
-                        <p>{car.model}</p>
-                    </div>
-                    <div>
-                        <label className="block text-gray-700 font-bold mb-2">Año</label>
-                        <p>{car.year}</p>
-                    </div>
-                </div>
-                <div className="overflow-auto">
-                    <div className="grid grid-cols-2 gap-4 mb-4">
-
+                <div className="px-10">
+                    <div className="grid grid-cols-3 gap-4 mb-4 mt-4">
                         <div>
-                            <label className="block text-gray-700 font-bold mb-2">Kilometraje</label>
+                            <label className="block text-gray-700 font-bold">Marca</label>
+                            <p>{car.brand}</p>
+                        </div>
+                        <div>
+                            <label className="block text-gray-700 font-bold">Modelo</label>
+                            <p>{car.model}</p>
+                        </div>
+                        <div>
+                            <label className="block text-gray-700 font-bold">Año</label>
+                            <p>{car.year}</p>
+                        </div>
+                        <div>
+                            <label className="block text-gray-700 font-bold">Kilometraje</label>
                             <p>{car.mileage}</p>
                         </div>
                         <div>
-                            <label className="block text-gray-700 font-bold mb-2">Precio</label>
+                            <label className="block text-gray-700 font-bold">Precio</label>
                             <p>${car.price}</p>
                         </div>
                         <div>
-                            <label className="block text-gray-700 font-bold mb-2">Tipo de Combustible</label>
+                            <label className="block text-gray-700 font-bold">Tipo de Combustible</label>
                             <p>{car.fuelType}</p>
                         </div>
                         <div>
-                            <label className="block text-gray-700 font-bold mb-2">Transmisión</label>
+                            <label className="block text-gray-700 font-bold">Transmisión</label>
                             <p>{car.transmission}</p>
                         </div>
                         <div>
-                            <label className="block text-gray-700 font-bold mb-2">Cilindrada del Motor</label>
+                            <label className="block text-gray-700 font-bold">Cilindrada del Motor</label>
                             <p>{car.engineCapacity}</p>
                         </div>
                         <div>
-                            <label className="block text-gray-700 font-bold mb-2">Condición</label>
+                            <label className="block text-gray-700 font-bold">Condición</label>
                             <p>{car.condition}</p>
                         </div>
-                        <div className="col-span-2">
-                            <label className="block text-gray-700 font-bold mb-2">Descripción</label>
-                            <p>{car.description}</p>
-                        </div>
                     </div>
+
                 </div>
+
+            </div>
+            <div className="max-w-[500px] w-full overflow-y-auto overflow-x-hidden px-10 pb-5">
+                <label className="block text-gray-700 font-bold">Descripción</label>
+                <p className="block max-w-[500px]">{car.description}</p>
             </div>
         </div>
     );
